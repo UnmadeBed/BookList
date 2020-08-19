09739a21083ff6898cc891800005ba15448ae40f
unmadebed <kevin.archer@live.com>
2020-08-20 00:53:25 +0100

    add ability to add a book to the book list & clear input fields

--- app.js
+++ app.js
@@ -42,6 +42,12 @@ class UI {
 
     list.appendChild(row);
   }
+
+  static clearFields() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#isbn').value = '';
+  }
 }
 
 // Store Class: Hanldes storage.
@@ -50,5 +56,23 @@ class UI {
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  // Prevent actual submit.
+  e.preventDefault();
+
+  // Get form values.
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const isbn = document.querySelector('#isbn').value;
+
+  // Instanitate book.
+  const book = new Book(title, author, isbn);
+
+  // Add book to UI.
+  UI.addBookToList(book);
+
+  // Clear fields.
+  UI.clearFields();
+});
 
 // Event: Remove a book.
